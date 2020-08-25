fa09013f295f18a434c2a9667fb5fdf28150a320
yaska1706 <yasinkinyua@gmail.com>
2020-08-25 13:59:01 +0300

    using online csv

--- src/cmds/check.js
+++ src/cmds/check.js
@@ -1,43 +1,42 @@
 module.exports = (args) => {
     //include readline to allow user input
     const rl = require('readline');
+    //converts csv to json
+    const csv=require('csvtojson')
+    const request = require('request')
 
     //function to allow user input
     function prompt(question) {
-        const r = rl.createInterface({
-          input: process.stdin,
-          output: process.stdout,
-          terminal: false
-        });
-        return new Promise((resolve, error) => {
-          r.question(question, answer => {
-            r.close()
-            resolve(answer)
-          });
-          console.error(error)
-        })
-      }
+            const r = rl.createInterface({
+            input: process.stdin,
+            output: process.stdout,
+            terminal: false
+            });
+            return new Promise((resolve, error) => {
+            r.question(question, answer => {
+                r.close()
+                resolve(answer)
+            });
+            console.error(error)
+            })
+        }
       
       //function prompts user to enter iso code and verify if it exists
       function check(){
-        const q = ('Enter ISO 4217 Code : ')
-        prompt(q).then(iso => {
-            //converts csv to json
-            const csv=require('csvtojson')
-
-            // Invoking csv to return a promise with user input
-            const converter=csv()
-            .fromFile('./Currencies.csv')
-            .then((json)=>{
-                for(var i=0;i<json.length;++i){
-                    if(json[i]['ISO 4217 Code'] === iso ){
-                        console.log(`${iso} is ISO 4217 Code for `+json[i].Country)
-                    }
-                   
-                }
-                
-            })
-        })
-      }
+                const q = ('Enter ISO 4217 Code : ')
+                prompt(q).then(iso => {
+                    csv()
+                    .fromStream(request.get('https://focusmobile-interview-materials.s3.eu-west-3.amazonaws.com/Cheap.Stocks.Internationalization.Currencies.csv'))
+                    .then((json)=>{
+                            for(var i=0;i<json.length;++i){
+                                if(json[i]['ISO 4217 Code'] === iso ){
+                                    console.log(`${iso} is ISO 4217 Code for `+json[i].Country)
+                                }
+                            
+                            }
+                        })
+                 
+                })
+        }
       check()
 }
--- src/cmds/table.js
+++ src/cmds/table.js
@@ -7,32 +7,31 @@ module.exports = (args) => {
     
 // Invoking csv returns a promise
     const converter=csv()
-    .fromFile('./Currencies.csv')
-    .then((json)=>{
-       
-        //create table using cli-table
-        const table = new Table({
-            head: ['Country', 'Currency', 'ISO 4217 code'],
-            colWidths: [20, 23, 8,],
-            wordWrap: true
-          });
-          
-            for(var i = 0; i < json.length; ++i){
-               //push values from csv to table 
-                table.push([
 
-                    json[i].Country,
-                    json[i].Currency,
-                    json[i]["ISO 4217 Code"]
-                ]);
+    csv()
+        .fromStream(request.get('https://focusmobile-interview-materials.s3.eu-west-3.amazonaws.com/Cheap.Stocks.Internationalization.Currencies.csv'))
+        .then((json)=>{
+            const table = new Table({
+                head: ['Country', 'Currency', 'ISO 4217 code'],
+                colWidths: [20, 23, 8,],
+                wordWrap: true
+              });
+              
+                for(var i = 0; i < json.length; ++i){
+                   //push values from csv to table 
+                    table.push([
+    
+                        json[i].Country,
+                        json[i].Currency,
+                        json[i]["ISO 4217 Code"]
+                    ]);
+    
+                }
+    
+              //display the table
+              console.log(table.toString());
 
-            }
-
-          //display the table
-          console.log(table.toString());
-       
-        
-    })
+        })
    
 
     
